37ac9755cea621449be28249ba5cf8df37613913
Henrique Lopes <39716479+Henrique0498@users.noreply.github.com>
2022-07-12 00:00:18 -0300

    Update - Valid password

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -5,13 +5,17 @@ import {
   HttpStatus,
   Post,
 } from '@nestjs/common';
+import { compare, hash } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
+import { UserService } from 'src/user/user.service';
 import { AuthenticateDto } from './dto/authenticate.dto';
 
 @Controller('api/v1/authentication')
 export class AuthenticationController {
+  constructor(private userService: UserService) {}
+
   @Post()
-  authenticateUser(@Body() authenticateDto: AuthenticateDto) {
+  async authenticateUser(@Body() authenticateDto: AuthenticateDto) {
     if (!authenticateDto.username || !authenticateDto.password) {
       throw new HttpException(
         {
@@ -21,12 +25,34 @@ export class AuthenticationController {
         HttpStatus.UNAUTHORIZED,
       );
     } else {
+      const userRequest = await this.userService.getUser(
+        authenticateDto.username,
+      );
+      const passwordIsValid = await compare(
+        authenticateDto.password,
+        userRequest.password,
+      );
+
+      if (!passwordIsValid) {
+        throw new HttpException(
+          {
+            status: HttpStatus.BAD_REQUEST,
+            error: 'A senha informada está incorreta.',
+          },
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+
       const token = sign({}, '3877d991-142e-44b2-9b95-d05e2120d0c9', {
         subject: authenticateDto.username,
         expiresIn: '20s',
       });
 
       return {
+        username: userRequest.username,
+        name: userRequest.name,
+        photo: userRequest.userPhoto,
+        cardPhoto: userRequest.cardPhoto,
         token: `Bearer ${token}`,
       };
     }
